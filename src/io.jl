3a1fd622ad7e9cb00df56425fe202fd33b30277c
Christopher <129872252+damourChris@users.noreply.github.com>
2025-10-21 00:00:03 +0200

    fix: add missing import and had a check if openslide_open fails

--- src/io.jl
+++ src/io.jl
@@ -1,4 +1,11 @@
+import SlideDiskArrays.LibOpenSlide: openslide_open
+
 function load(filepath::AbstractString; level::Int = 0)
     osr = openslide_open(filepath)
+
+    if osr == C_NULL
+        throw(ErrorException("Failed to open slide image at $filepath"))
+    end
+
     return SlideDiskArray(osr, level)
 end
