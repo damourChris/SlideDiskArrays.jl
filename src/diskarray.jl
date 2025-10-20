ff239917cc9e8a6f4c2c68585a373f8f2404cda9
Christopher <129872252+damourChris@users.noreply.github.com>
2025-10-20 20:59:11 +0200

    feat: implement SlideDiskArray structure and DiskArrays interface

---
+++ src/diskarray.jl
@@ -0,0 +1,57 @@
+using DiskArrays
+using Images
+import SlideDiskArrays.LibOpenSlide:
+    openslide_t, openslide_get_level_dimensions, openslide_read_region, openslide_close
+
+struct SlideDiskArray{T,N} <: DiskArrays.AbstractDiskArray{T,N}
+    osr::Ptr{openslide_t}
+    level::Int
+    size::NTuple{N,Int}
+
+    function SlideDiskArray(osr::Ptr{openslide_t}, level::Int)
+        # Allocate the w, h
+        w = Ref{Int64}()
+        h = Ref{Int64}()
+
+        openslide_get_level_dimensions(osr, level, w, h)
+
+        # This is nessecary since data read by openslide is in ARGB format
+        # The data returned by read_region is in UInt32 format so the associated Images.jl type
+        # is RGB24
+        T = RGB24
+
+        slide_disk_array = SlideDiskArray{T,2}(osr, level, (w[], h[]))
+        finalizer(openslide_close, osr)
+        return slide_disk_array
+    end
+end
+
+
+# DiskArrays interface
+
+Base.size(A::SlideDiskArray) = A.size
+DiskArrays.haschunks(A::SlideDiskArray) = DiskArrays.Chunked()
+DiskArrays.eachchunk(A::SlideDiskArray) = DiskArrays.GridChunks(A, size(A))
+
+function DiskArrays.readblock!(
+    A::SlideDiskArray{T,2},
+    aout,
+    r::Vararg{AbstractUnitRange,2},
+) where {T}
+    xrange, yrange = r
+    x = first(xrange) - 1
+    y = first(yrange) - 1
+    w = length(xrange)
+    h = length(yrange)
+
+    # Reshape the input array to be a UInt32 view to pass to openslide
+    aout_view = reinterpret(reshape, UInt32, aout)
+
+    # Shift the view to be 1D
+    aout_view = reshape(aout_view, :)
+
+    openslide_read_region(A.osr, aout_view, x, y, A.level, w, h)
+
+    return aout_view
+end
+
